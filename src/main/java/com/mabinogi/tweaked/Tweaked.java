83e7022afc461dfba76ecaf83a5a04669b220827
Mabinogi <MabinogiGaming@gmail.com>
2018-10-30 15:55:58 +0000

    v0.2.4
    Sorted mcmod.info #38

--- src/main/java/com/mabinogi/tweaked/Tweaked.java
+++ src/main/java/com/mabinogi/tweaked/Tweaked.java
@@ -24,7 +24,7 @@ public class Tweaked
 {
     public static final String MODID = "tweaked";
     public static final String NAME = "Tweaked";
-    public static final String VERSION = "0.2.3";
+    public static final String VERSION = "0.2.4";
     
     @Instance
     public static Tweaked instance;
