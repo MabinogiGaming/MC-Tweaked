5cd2b98b24b69ce766a619c7cd8ffa0b6873b7af
Mabinogi <MabinogiGaming@gmail.com>
2019-01-16 14:00:05 +0000

    v0.3.9
    Added Chat Command - Lang #67
    Added Documentation - First Script page #42
    Cleaned Documentation - Redesign #63

---
+++ src/main/java/com/mabinogi/tweaked/commands/CommandLang.java
@@ -0,0 +1,60 @@
+package com.mabinogi.tweaked.commands;
+
+import com.mabinogi.tweaked.api.annotations.TweakedCommand;
+import com.mabinogi.tweaked.api.commands.ITweakedCommand;
+import com.mabinogi.tweaked.helpers.CommandHelper;
+import com.mabinogi.tweaked.script.ScriptHelper;
+import net.minecraft.entity.player.EntityPlayer;
+import net.minecraft.item.ItemStack;
+import net.minecraft.server.MinecraftServer;
+import net.minecraft.util.math.BlockPos;
+
+import java.util.Collections;
+import java.util.List;
+
+import static com.mabinogi.tweaked.Tweaked.LOG;
+import static com.mabinogi.tweaked.controllers.TweakedLogging.TAB;
+
+@TweakedCommand("lang")
+public class CommandLang implements ITweakedCommand
+{
+	@Override
+	public boolean isHidden()
+	{
+		return false;
+	}
+	
+	@Override
+	public void execute(MinecraftServer server, EntityPlayer player, String[] args)
+	{
+		//get held item
+        ItemStack heldItem = player.getHeldItemMainhand();
+        
+        // Tries to get name of held item first
+        if(!heldItem.isEmpty()) 
+        {
+        	//dump the message
+    		LOG.dump("/tweaked lang");
+    		
+    		//create a script version of the stack
+    		String out = heldItem.getUnlocalizedName() + ".name";
+
+    		//reply
+    		CommandHelper.sendCopyMessage(player, out);
+			
+            //dump the message
+			LOG.dump(TAB + out);
+        }
+        else
+        {
+			CommandHelper.sendMessage(player, "Requires an item in the main hand");
+        }
+	}
+
+	@Override
+	public List<String> getTabCompletions(MinecraftServer server, EntityPlayer player, String[] args, BlockPos targetPos)
+	{
+		return Collections.emptyList();
+	}
+
+}
